8c0b89feae24aeb64005d4a8aad31cb2c224249b
Kannan S <45405245+kannansgithub@users.noreply.github.com>
2020-10-24 22:02:51 +0530

    Cleaned Code

---
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: [emailField(), passwordField()],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'Email Address',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Password',
+      ),
+    );
+  }
+}
